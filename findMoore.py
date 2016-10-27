c3214f7f7ade5c81446ba6599b99b22e8165edff
Austin Williams <austinwilliams123@gmail.com>
2016-10-27 13:55:28 -0700

    make basic algorithm functional and ready to deploy

--- findMoore.py
+++ findMoore.py
@@ -31,8 +31,7 @@ class Initialiser:
 			wb.new_basket()
 			# Optional: run an accelerator to get the known-present edges in without running through the huristic checks
 			if self.accelerate:
-				#run accelerator protocol # todo
-				pass
+				accelerate() # todo
 
 		elif self.continue_file:
 			# if continuing an old one then
@@ -80,7 +79,12 @@ class WorkingBasket:
 		# computes the degree of vertex in the current working basket
 		return (self.matrix[vertex] > 0).sum()
 
+	def pot_degree_of(self, vertex):
+		# computes the number of potential edges at vertex
+		return (self.matrix[vertex] == 0).sum()
+
 	def _flatten(self, value):
+		# !critical component of feasibility_check_2() -- do not alter
 		if value < 0:
 			return False
 		else:
@@ -95,10 +99,10 @@ class Manager:
 
 	def search(self):
 		while True:
+			print 'edge_label is ' + str(wb.current_edge_label)
 			# EdgePicker tries to pick an available edge to label WorkingBasket.current_edge_label
 			try:
 				new_edge = choose_new_edge('deep')
-
 			except EdgesExhausted: 
 				# if there are no more available edges to pick, then we're finished and we need to check
 				#  whether we've found a Moore graph or whether one isn't possible
@@ -128,8 +132,8 @@ class Manager:
 					# mark newly unavailable edges with -WorkingBasket.current_edge_label
 					label_non_edges(new_edge, -wb.current_edge_label)
 					# progress is saved / WorkingBasket is pickled
-					saver.save()
-			break # todo remove this break, this is just to prevent endless looping while the rest of the program is absent.
+					if (wb.current_edge_label % 1000) == 0:
+						saver.save()
 
 class HeuristicConductor:
 	def __init__(self):
@@ -142,19 +146,19 @@ class HeuristicConductor:
 
 def choose_new_edge(mode):
 	global wb
-	available_edges = []
-	edge = None
-	# compute set of available edges and store it as available_edges
-		# todo
-	# if no edges are available then raise exception
-	if not available_edges:
-		raise EdgesExhausted()
+
+	if not any((any((x == 0 for x in wb.matrix[y])) for y in range(wb.v))):
+		raise EdgesExhausted('Edges exhasted')
 	else:
 		# select the new edge based on mode (deep, wide, or random). Use deep by default. Return a tuple.
-		# todo
-		pass # todo
-
-	return edge
+		if mode == 'deep':
+			# return a vertex that has the least number of potential edge ends
+			pot_degree_list = [wb.pot_degree_of(v) for v in range(wb.v)]
+			v0 = pot_degree_list.index(min([x for x in pot_degree_list if x !=0]))
+			v1 = [v for v in wb.matrix[v0]].index(0)
+			return (v0,v1)
+		else:
+			sys.exit("[!] choose_new_edge(mode) has invalid mode set.")
 
 class EdgesExhausted(Exception):
 	# this exception is raised by choose_new_edge if there are no avaialable edges from which to choose
@@ -186,9 +190,9 @@ class Verifier:
 		# degree self.val
 		self.degree_check = all([d == self.val for d in self.candidate_graph.graph.degree()])
 		# order self.v
-		self.order_check = self.candidate_graph.graph.vcount == self.v
+		self.order_check = self.candidate_graph.graph.vcount() == self.v
 		# edge count
-		self.edge_check = self.candidate_graph.graph.ecount == self.e
+		self.edge_check = self.candidate_graph.graph.ecount() == self.e
 		# return Moore graph staus
 		return self.diameter_check and self.girth_check and self.degree_check and self.order_check and self.edge_check
 
@@ -207,7 +211,11 @@ class CandidateGraph:
 	def build_adj_mat(self):
 		# use self.basket to create an adjacency matrix for the complete candidate graph 
 		self.adj_mat = np.vectorize(self._project, otypes=[np.int8])(self.basket) # gives the adj matrix of the basket
-		self.adj_mat.resize((self.v, self.v)) # adds (self.val + 1) more rows/cols to adj_mat, all zeroed out
+		# add (self.val + 1) more rows/cols to adj_mat, all zeroed out
+		b = np.zeros((self.v,self.v))
+		b[:-(self.val + 1),:-(self.val + 1)] = self.adj_mat
+		self.adj_mat = np.array(b, dtype=np.int32)
+	
 		parent_verts = tuple(i for i in range(self.basket_v,self.basket_v + self.val))
 		# connect each parent vertex to each vertex in it's corresponding fruit
 		parent_edges = it.chain.from_iterable(tuple(tuple((parent_verts[p],v) for v in self.fruit[p]) for p in range(self.val)))
@@ -235,7 +243,7 @@ def endgame():
 		# we found a Moore graph!
 		# save the adjacencyMatrix
 		filename = 'moore_graph-val-' + str(wb.val) + "-" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")+".p"
-		pickle.dump(list(c.graph.get_adjacency()) , open(filename, "wb"))
+		pickle.dump(list(candidate_graph.graph.get_adjacency()) , open(filename, "wb"))
 		sys.exit("[!] A Moore graph was found and saved.")
 	else: # not a moore graph.
 		sys.exit("[!] No Moore graph was found.")
@@ -251,11 +259,14 @@ def label_non_edges(new_edge, label):
 	## Make negative the edges that would cause a vertex to have degree > wb.val-1
 	#    note that the addition of new_edge will only have raised the degree of the vertices new_edge[0] and new_edge[1], so we need
 	#     only check those vertices
-	
 	for edge_end in range(2):
 		if wb.degree_of(new_edge[edge_end]) == wb.val -1: # then no more edges can be added to vertex new_edge[0]
 			# so we find all potential edges at new_edge[0] and label them -wb.current_edge_label
 			[wb.label((new_edge[edge_end],v), label) for v in np.where(wb.matrix[new_edge[edge_end]] == 0)[0]]
+
+	## Make negative all other edges from v0 to fruit_of(v1), and all other edges from v1 to fruit_of(v0) !!
+	for edge_end in range(2):
+		[wb.label((new_edge[edge_end],v),-wb.current_edge_label)for v in fruit_of(new_edge[(edge_end + 1)%2]) if wb.matrix[new_edge[edge_end]][v] == 0]
 
 	## Make negative the edges that would create triangles
 	#    begin with the vertex new_edge[0] and look at every vertex already incident to edge[0]
@@ -263,7 +274,6 @@ def label_non_edges(new_edge, label):
 	# if wb.matrix[vertex][new_edge[1]] is zero the mark that edge negative (because adding it would cause a triangle to form)
 	for edge_end in range(2):	
 		[wb.label((neighbor,new_edge[(edge_end + 1)%2]),label) for neighbor in np.where(wb.matrix[new_edge[edge_end]] > 0)[0] if wb.matrix[neighbor][new_edge[(edge_end + 1)%2]] == 0]
-	
 	## Make negative the edges that would create rectangles (three cases)
 	# case 1
 	#	 suppose new_edge = (v0, v1)
@@ -271,8 +281,7 @@ def label_non_edges(new_edge, label):
 	#    find the neighbors(v1). They are np.where(wb.matrix[new_edge[1]] > 0)[0]
 	#	 we want to rule out all edges (i,j) where i is in neighbors(v0) and j is in neighbors(v1), because those edges would cause
 	#     a rectangle to form
-	
-	[ wb.label((i,j),label) for (i,j) in ((i,j) for i in np.where(wb.matrix[new_edge[0]] > 0)[0] for j in np.where(wb.matrix[new_edge[1]] > 0)[0]) if wb.matrix[i][j] == 0]
+	[wb.label((i,j),label) for (i,j) in ((i,j) for i in np.where(wb.matrix[new_edge[0]] > 0)[0] for j in np.where(wb.matrix[new_edge[1]] > 0)[0]) if wb.matrix[i][j] == 0]
 
 	# cases 2 & 3
 	#    suppose new_edge = (v0, v1)
@@ -304,6 +313,14 @@ def feasibility_check_2():
 	[[[label_true(intra_fruit_matrix,e[0],e[1]) for e in [(i,j) for i in fruit for j in fruit]] for v in fruit] for fruit in wb.fruit]
 	return (neighbors + neighbors_of_neighbors + intra_fruit_matrix).all()
 
+def accelerate():
+	# for all v in fruit(0), place an edge from v to the vth edge in every other fruit
+	for v in wb.fruit[0]:
+		for f in range(1,wb.val):
+			wb.current_edge_label += 1
+			wb.label((v,wb.fruit[f][v]), wb.current_edge_label)
+			label_non_edges((v,wb.fruit[f][v]), -wb.current_edge_label)
+	print 'accelerate() complete'
 
 def label_true(matrix, i, j):
 	matrix[i][j] = matrix[j][i] = True
@@ -332,6 +349,13 @@ def test_setup(): # todo remove this function
 	wb.label((0,2),4)
 	wb.label((2,3),4)
 	new_edge = (0,2)
+
+def test_setup_2(): # todo remove this function
+	global wb
+	global candidate_graph
+	global verifier
+	candidate_graph = CandidateGraph(wb.matrix)
+	verifier = Verifier(wb.val, candidate_graph)	
 
 
 if __name__ == "__main__":
